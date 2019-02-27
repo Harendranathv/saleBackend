18733b7b483d9fe1da71bf403e65e4f4645a1b83
Phat Hong Phung <Phat.hongPhung@nashtechglobal.com>
2019-02-27 11:09:39 +0700

    Add more API for get and post products

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const dataFilePath = './data/products.json';
 
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const fs = require('fs');
+const bodyParser = require('body-parser');
+let rawdata = fs.readFileSync(dataFilePath);
+let products = JSON.parse(rawdata);
 express()
+    .use(bodyParser())
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
-    .get('/app', (req, res) => res.json({abc: 1}))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .get('/products', (req, res) => res.json(products))
+    .post('/products', (req, res) => {
+        let newProduct = req.body;
+        newProduct.id = products.length + 1;
+        products.push(newProduct);
+        fs.writeFile(dataFilePath, JSON.stringify(products), ()=>{
+        });
+        res.json(products);
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
